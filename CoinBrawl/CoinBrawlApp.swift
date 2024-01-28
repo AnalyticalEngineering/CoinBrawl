278a08c85a843fed85d0c87c3f579391c1ace5d4
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-28 10:41:30 -0600

    Initial Commit

---
+++ CoinBrawl/CoinBrawlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinBrawlApp.swift
+//  CoinBrawl
+//
+//  Created by J. DeWeese on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinBrawlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
